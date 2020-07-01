482f942f42acf425e74fdd2e0d987e28024caeb9
igokul95 <gokul@perleybrook.com>
2020-07-01 23:30:22 +0530

    Updates

---
+++ precache-manifest.5053300418880e7e47d1b929faa0ee30.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d51a53b0f03ee30c3a2b602a3f8e56b0",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "f05ffc4760d0a52ee137",
+    "url": "/myapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a6a47aea6471001ad92b",
+    "url": "/myapp/static/js/2.704ff7d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myapp/static/js/2.704ff7d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f05ffc4760d0a52ee137",
+    "url": "/myapp/static/js/main.ecac2d2e.chunk.js"
+  },
+  {
+    "revision": "65f1855b6321dd35f25a",
+    "url": "/myapp/static/js/runtime-main.fc08ab31.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
